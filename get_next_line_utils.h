19ffbc8c17454da8c09daa35fd7945be7d36ccfe
tpereira <tpereira@student.42lisboa.com>
2021-03-07 17:01:17 +0000

    get next line

--- get_next_line_utils.h
+++ get_next_line_utils.h
@@ -1,24 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_utils.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/06 17:04:32 by tpereira          #+#    #+#             */
-/*   Updated: 2021/03/07 16:59:58 by tpereira         ###   ########.fr       */
+/*   Created: 2021/03/07 11:47:27 by tpereira          #+#    #+#             */
+/*   Updated: 2021/03/07 12:05:13 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line_utils.h"
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-int get_next_line(int fd, char **line)
-{
-	while (*line)
-	{
-		read(fd, buf, BUFFER_SIZE);
-		line++;
-	}
 
-	return (0);
-}
+
+#endif
