bc43bb1ff7781c523cfe331a1a2392bf3708f7b1
Tiago <tpereira@c1r11s1.42lisboa.com>
2021-03-13 18:13:46 +0000

    get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,22 +6,72 @@
 /*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 17:04:32 by tpereira          #+#    #+#             */
-/*   Updated: 2021/03/13 14:12:23 by tpereira         ###   ########.fr       */
+/*   Updated: 2021/03/13 18:13:26 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+static	int	ft_saveline(char **save, char **line)
+{
+	int	len;
+	char **temp;
+
+	len = 0;
+	while ((*save)[len] != '\n' && (*s)[len] != '\0')
+		len++;
+	if ((*save)[len] == '\n')
+	{
+		*line = ft_substr(*save, 0, len);
+		temp = ft_strdup(&((*save)[len + ]1));
+		free(*save);
+		*save = temp;
+	}
+	else
+	{
+		*line = ft_strdup(*save);
+		free(*save);
+		*save = NULL;
+		return (0);
+	}
+	return (1);
+}
+
+static	int	ft_output(char **save, char **line, int r, int fd)
+{
+	if (r < 0)
+		return (-1);
+	else if (r == 0 && save[fd] == NULL)
+	{
+		*line = ft_strdup("");
+		return ;
+	}
+	else
+		return (ft_saveline(&s[fd], line));
+}
+
 int get_next_line(int fd, char **line)
 {
-	char *temp;
-	
-	temp = NULL;
-	while (*line)
+	int			r;
+	static char	save[OPEN_MAX];
+	char 		*temp;
+	char 		buff[BUFFER_SIZE];
+//↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ se houver erros return -1 ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+	if (fd < 0 || BUFFER_SIZE <= 0 || !line || read(fd, buff, 0) < 0)
+		return (-1);
+	while ((r = read(fd, buff, BUFFER_SIZE)) > 0)
 	{
-		read(fd, temp, BUFFER_SIZE);
-		*line = temp;
-		line++;
+		buff[r] = '\0';
+		if (save[fd] == NULL)
+			save[fd] = ft_strdup(buff);
+		else
+		{
+			temp = ft_strjoin(save[fd], buff);
+			free(save[fd]);
+			save[fd] = temp;
+		}
+		if (ft_strchr(save[fd], '\n'))
+			break ;
 	}
-	return (0);
+	return (ft_output(save, line, r, fd));
 }
